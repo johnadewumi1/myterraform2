8d2180cc5fa7a108eed0455d5bc2fc6142167ced
John Adewumi <johnadewumi63@gmail.com>
2021-06-14 13:33:23 -0700

    new

--- bastionsubnet.tf
+++ bastionsubnet.tf
@@ -5,6 +5,6 @@ resource "oci_core_subnet" "bastionsubnet" {
   vcn_id            = oci_core_vcn.johnvcn.id
   dns_label         = "bastiondns"
   route_table_id    = oci_core_route_table.john_route_table.id
-  dhcp_options_id   = oci_core_dhcp_options.johnDhcpOptions1.id
+  dhcp_options_id   = oci_core_dhcp_options.johnDhcpOptions.id
   security_list_ids = [oci_core_security_list.sshsecuritylist.id]
 }
